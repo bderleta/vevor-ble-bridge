2a4695d665f8671cbcdff64fec0a1572f213b413
Bartosz Derleta <bartosz@derleta.com>
2024-01-31 22:11:31 +0000

    Initial commit

---
+++ VevorDieselHeater.py
@@ -0,0 +1,179 @@
+import os
+from bluepy.btle import Peripheral, DefaultDelegate
+import threading
+import time
+import random
+import math
+import struct
+
+def _u8tonumber(e):
+    return (e + 256) if (e < 0) else e
+
+def _UnsignToSign(e):
+    if (e > 32767.5):
+        e = e | -65536
+    return e
+
+class _VevorDieselHeaterNotification:
+    _error_strings = (
+        "No fault",
+        "Startup failure",
+        "Lack of fuel",
+        "Supply voltage overrun",
+        "Outlet sensor fault",
+        "Inlet sensor fault",
+        "Pulse pump fault",
+        "Fan fault",
+        "Ignition unit fault",
+        "Overheating",
+        "Overheat sensor fault"
+    )
+    _error_strings_alt = (
+        "No fault",
+        "Supply voltage overrun",
+        None,
+        "Ignition unit fault",
+        "Pulse pump fault",
+        "Overheating",
+        "Fan fault",
+        None,
+        "Lack of fuel",
+        "Overheat sensor fault",
+        "Startup failure"
+    )
+    _running_step_strings = (
+        "Heating",
+        "Run Self-test",
+        "Ignition Preparation",
+        "Stable Combustion",
+        "Shutdown Cooling"
+    )
+    
+    def __init__(self, je):
+        print("< " + je.hex(' ', 1))
+        fb = _u8tonumber(je[0])
+        sb = _u8tonumber(je[1])
+        if (170 == fb) and (85 == sb):
+            self.running_state = _u8tonumber(je[3]) # Is running at all?
+            self.error = _u8tonumber(je[4])
+            self.error_msg = self._error_strings[self.error]
+            self.running_step = _u8tonumber(je[5]) # Detailed state when running
+            self.running_step_msg = self._running_step_strings[self.running_step]
+            self.altitude = _u8tonumber(je[6]) + 256 * _u8tonumber(je[7])
+            self.running_mode = _u8tonumber(je[8]) # Temperature / Level mode
+            match self.running_mode:
+                case 0:
+                    self.set_level = _u8tonumber(je[10]) + 1
+                    self.set_temperature = None
+                case 1:
+                    self.set_level = _u8tonumber(je[9])
+                    self.set_temperature = None
+                case 2:
+                    self.set_temperature = _u8tonumber(je[9])
+                    self.set_level = _u8tonumber(je[10]) + 1
+                case _:
+                    raise RuntimeError('Unrecognized running mode')
+            self.supply_voltage = ((256 * _u8tonumber(je[12]) + _u8tonumber(je[11])) / 10) 
+            self.case_temperature = _UnsignToSign(256 * je[14] + je[13])
+            self.cab_temperature =  _UnsignToSign(256 * je[16] + je[15])
+            self.md = 1
+        elif (170 == fb) and (102 == sb):
+            self.running_state = _u8tonumber(je[3])
+            self.error = _u8tonumber(je[17])
+            self.error_msg = self._error_strings_alt[self.error]
+            self.running_step = _u8tonumber(je[5])
+            self.running_step_msg = self._running_step_strings[self.running_step]
+            self.altitude = _u8tonumber(je[6]) + 256 * _u8tonumber(je[7])
+            self.running_mode = _u8tonumber(je[8])
+            match self.running_mode:
+                case 0:
+                    self.set_level = _u8tonumber(je[10]) + 1
+                    self.set_temperature = None
+                case 1:
+                    self.set_level = _u8tonumber(je[9])
+                    self.set_temperature = None
+                case 2:
+                    self.set_temperature = _u8tonumber(je[9])
+                    self.set_level = _u8tonumber(je[10]) + 1
+                case _:
+                    raise RuntimeError('Unrecognized running mode')
+            self.supply_voltage = ((256 * _u8tonumber(je[12]) + _u8tonumber(je[11])) / 10) 
+            self.case_temperature = _UnsignToSign(256 * je[14] + je[13])
+            self.cab_temperature =  _UnsignToSign(256 * je[16] + je[15])
+            self.md = 3
+        elif (170 == fb) and (136 == sb):
+            raise RuntimeError('Unsupported payload (todo)')
+        else:
+            raise RuntimeError('Unrecognized payload')
+    
+    def dump(self):
+        print(vars(self))
+        
+class _VevorDieselHeaterDelegate(DefaultDelegate):
+    def __init__(self, parent):
+        self.parent = parent
+    
+    def handleNotification(self, cHandle, data):
+        self.parent._last_notification = _VevorDieselHeaterNotification(data)
+
+class VevorDieselHeater:
+    _service_uuid = "0000ffe0-0000-1000-8000-00805f9b34fb"
+    _characteristic_uuid = "0000ffe1-0000-1000-8000-00805f9b34fb"
+    _last_notification = None
+    
+    def __init__(self, mac_address: str, passkey: int):
+        self.mac_address = mac_address
+        self.passkey = passkey
+        self.peripheral = Peripheral(mac_address, "public")
+        self.service = self.peripheral.getServiceByUUID(self._service_uuid)
+        if self.service is None:
+            raise RuntimeError('Requested service is not supported by peripheral');
+        self.characteristic = self.service.getCharacteristics(self._characteristic_uuid)[0]
+        if self.characteristic is None:
+            raise RuntimeError('Requested characteristic is not supported by service');
+        self.peripheral.setDelegate(_VevorDieselHeaterDelegate(self))
+        
+    def _send_command(self, command: int, argument: int, n: int):
+        o = bytearray([0xaa, n % 256, 0, 0, 0, 0, 0, 0])
+        if 136 == n:
+            o[2] = random.randint(0, 255)
+            o[3] = random.randint(0, 255)
+        else: # 85
+            o[2] = math.floor(self.passkey / 100)
+            o[3] = self.passkey % 100
+        o[4] = command % 256
+        o[5] = argument % 256
+        o[6] = math.floor(argument / 256)
+        o[7] = o[2] + o[3] + o[4] + o[5] + o[6]
+        print("> " + o.hex(' ', 1))
+        last_notification = None
+        response = self.characteristic.write(o, withResponse=True) # returns sth like "{'rsp': ['wr']}"
+        if (self.peripheral.waitForNotifications(1) and self._last_notification):
+            ln = self._last_notification
+            self._last_notification = None
+            return ln
+        return None
+        
+    def get_status(self):
+        # todo: mode 136
+        return self._send_command(1, 0, 85)
+        
+    def start(self):
+        return self._send_command(3, 1, 85)
+        
+    def stop(self):
+        return self._send_command(3, 0, 85)
+        
+        
+vdh = VevorDieselHeater("1e:00:10:0a:d6:15", 2697)
+
+result = vdh.stop()
+if (result):
+	result.dump()
+	time.sleep(5)
+
+while True:
+    result = vdh.get_status()
+    if (result):
+        result.dump()
+        time.sleep(5)
